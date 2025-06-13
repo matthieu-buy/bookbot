4f8e9b0121f922cc8ce97718f09310b2c738cf13
matthieubuy <matthieu.buy56@gmail.com>
2025-06-13 12:02:57 +0200

    CH3 modify mainpy : report on multiple books.txt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from stats import get_characters_counts
 from stats import get_num_words
 from stats import order_list_of_characters
+import sys
+
 
 
 def get_book_text(path_to_file :str) :
@@ -25,12 +27,17 @@ def print_report(book_path :str, num_words :int, characters_list :list) :
 ###################
 
 def main() :
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_characters_counts(text)
-    chars_sorted_list = order_list_of_characters(chars_dict)
-    print_report(book_path, num_words, chars_sorted_list)
+    if len(sys.argv) < 2 :
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else :
+        # book_path = "books/frankenstein.txt"
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        chars_dict = get_characters_counts(text)
+        chars_sorted_list = order_list_of_characters(chars_dict)
+        print_report(book_path, num_words, chars_sorted_list)
 
 
 
